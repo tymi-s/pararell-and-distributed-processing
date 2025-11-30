aee2f71a27520e12dda010bb49cdb4d3d4251e3d
Tymon <tymonszyler@gmail.com>
2025-11-30 16:53:58 +0100

    lab4

---
+++ lab4/clasic.c
@@ -0,0 +1,105 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <time.h>
+#include<pthread.h>
+#include <unistd.h>
+
+#define ILE_MUSZE_WYPIC 3
+
+/////////////////////////////////////////////////////////
+int wolne_kufle;
+int poczatkowa_liczba_kufli; 
+//MUTEX CZYLI KŁÓDKA
+pthread_mutex_t mutex_kufel;
+
+
+
+void * watek_klient (void * arg);
+
+int main( void ){
+
+  pthread_t *tab_klient;
+  int *tab_klient_id;
+
+  int l_kl, l_kf, l_kr, i;
+
+  printf("\nLiczba klientow: "); scanf("%d", &l_kl);
+
+  printf("\nLiczba kufli: "); scanf("%d", &l_kf);
+
+  //printf("\nLiczba kranow: "); scanf("%d", &l_kr);
+  l_kr = 1000000000; // wystarczająco dużo, żeby nie było rywalizacji 
+
+/////////////////////////////////////////
+  wolne_kufle=l_kf;
+  poczatkowa_liczba_kufli =l_kf;
+  pthread_mutex_init(&mutex_kufel,NULL);
+  ////////////////////////////////////////
+
+  tab_klient = (pthread_t *) malloc(l_kl*sizeof(pthread_t));
+  tab_klient_id = (int *) malloc(l_kl*sizeof(int));
+  for(i=0;i<l_kl;i++) tab_klient_id[i]=i;
+
+
+  printf("\nOtwieramy pub (simple)!\n");
+  printf("\nLiczba wolnych kufli %d\n", l_kf); 
+
+  for(i=0;i<l_kl;i++){
+    pthread_create(&tab_klient[i], NULL, watek_klient, &tab_klient_id[i]); 
+  }
+  for(i=0;i<l_kl;i++){
+    pthread_join( tab_klient[i], NULL);
+  }
+  pthread_mutex_destroy(&mutex_kufel);
+  printf("\nZamykamy pub!\n");
+
+
+}
+
+
+void * watek_klient (void * arg_wsk){
+
+  int moj_id = * ((int *)arg_wsk);
+
+  int i, j, kufel, result;
+  int ile_musze_wypic = ILE_MUSZE_WYPIC;
+
+  long int wykonana_praca = 0;
+
+  printf("\nKlient %d, wchodzę do pubu\n", moj_id); 
+    
+  for(i=0; i<ile_musze_wypic; i++){
+    ////////////////////////////////////////////
+    printf("\nKlient %d, wybieram kufel\n", moj_id);
+     
+    wolne_kufle--;
+    
+    int aktualne_kufle = wolne_kufle;
+    
+    
+
+    if(aktualne_kufle<0){
+	printf("PRZYPAL! KLIENT %d POBRAL  KUFEL KTOREGO NIE MA ! liczba wolny kufli %d",moj_id,aktualne_kufle);
+    }    
+
+    j=0;
+    printf("\nKlient %d, nalewam z kranu %d\n", moj_id, j); 
+    usleep(30);
+    
+    printf("\nKlient %d, pije\n", moj_id); 
+    nanosleep((struct timespec[]){{0, 50000000L}}, NULL);
+    
+    printf("\nKlient %d, odkladam kufel\n", moj_id);
+    ////////////////////////////////////////////////////////////// 
+  
+    wolne_kufle++;
+    
+  }
+
+  printf("\nKlient %d, wychodzę z pubu; wykonana praca %ld\n",
+	 moj_id, wykonana_praca); 
+    
+  return(NULL);
+} 
+
+
